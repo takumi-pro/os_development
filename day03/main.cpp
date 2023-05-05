35ba15afe81b88c75f33cb53177b932d733d1ec2
takumi <takumidiary.0927@gmail.com>
2023-05-05 14:51:09 +0900

    drawing pixels

--- day03/main.cpp
+++ day03/main.cpp
@@ -1,4 +1,15 @@
+#include <cstdint>
 
-extern "C" void KernelMain() {
+extern "C" void KernelMain(
+  // ブートローダからのフレームバッファの先頭アドレス
+  uint64_t frame_buffer_base,
+  // ブートローダからのフレームバッファのサイズ
+  uint64_t frame_buffer_size
+) {
+  // frame_buffer_baseをuint8_tのポインタ型に変換
+  uint8_t* frame_buffer = reinterpret_cast<uint8_t*>(frame_buffer_base);
+  for (uint64_t i = 0; i < frame_buffer_size; ++i) {
+    frame_buffer[i] = i % 256;
+  }
   while (1) __asm__("hlt");
 }
