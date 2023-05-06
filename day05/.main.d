c9930cf5e7d9f5a0fdeb95efd5a43851a7f4067f
takumi <takumidiary.0927@gmail.com>
2023-05-06 15:12:34 +0900

    separate compilation

---
+++ day05/.main.d
@@ -0,0 +1,7 @@
+main.o: main.cpp \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/cstdint \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/__config \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/cstddef \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/version \
+  frame_buffer_config.hpp graphics.hpp font.hpp
