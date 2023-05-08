7de1b2705e61b489a047189229cedada924d2a6c
takumi <takumidiary.0927@gmail.com>
2023-05-08 21:38:39 +0900

    mouse cursor

---
+++ kernel/.main.d
@@ -0,0 +1,9 @@
+main.o: main.cpp \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/cstdint \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/__config \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/cstddef \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/version \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/cstdio \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/stdio.h \
+  frame_buffer_config.hpp graphics.hpp font.hpp console.hpp
