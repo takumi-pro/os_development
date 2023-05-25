699c421dd885ce22b3880520161aa9407aafdb7f
takumi <takumidiary.0927@gmail.com>
2023-05-26 00:05:19 +0900

    overlap

--- kernel/.console.d
+++ kernel/.console.d
@@ -3,4 +3,40 @@ console.o: console.cpp console.hpp graphics.hpp frame_buffer_config.hpp \
   /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/__config \
   /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/cstring \
   /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/string.h font.hpp \
-  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/cstdint
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/cstdint layer.hpp \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/memory \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/type_traits \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/cstddef \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/version \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/typeinfo \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/exception \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/cstdlib \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/stdlib.h \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/new \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/utility \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/__tuple \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/initializer_list \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/__debug \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/limits \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/iterator \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/iosfwd \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/wchar.h \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/__functional_base \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/tuple \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/stdexcept \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/cassert \
+  /home/takumi/osbook/devenv/x86_64-elf/include/assert.h \
+  /home/takumi/osbook/devenv/x86_64-elf/include/_ansi.h \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/map \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/__tree \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/algorithm \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/functional \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/bit \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/__node_handle \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/optional \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/vector \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/__bit_reference \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/climits \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/limits.h \
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/__split_buffer \
+  window.hpp
--- kernel/main.cpp
+++ kernel/main.cpp
@@ -30,9 +30,8 @@
 #include "segment.hpp"
 #include "paging.hpp"
 #include "memory_manager.hpp"
-
-const PixelColor kDesktopBGColor{45, 118, 237};
-const PixelColor kDesktopFGColor{255, 255, 255};
+#include "window.hpp"
+#include "layer.hpp"
 
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter* pixel_writer;
@@ -58,11 +57,11 @@ char memory_manager_buf[sizeof(BitmapMemoryManager)];
 BitmapMemoryManager* memory_manager;
 // #@@range_end(memman_buf)
 
-char mouse_cursor_buf[sizeof(MouseCursor)];
-MouseCursor* mouse_cursor;
+unsigned int mouse_layer_id;
 
 void MouseObserver(int8_t displacement_x, int8_t displacement_y) {
-  mouse_cursor->MoveRelative({displacement_x, displacement_y});
+  layer_manager->MoveRelative(mouse_layer_id, {displacement_x, displacement_y});
+  layer_manager->Draw();
 }
 
 void SwitchEhci2Xhci(const pci::Device& xhc_dev) {
@@ -121,29 +120,12 @@ extern "C" void KernelMainNewStack(
       break;
   }
 
-  const int kFrameWidth = frame_buffer_config.horizontal_resolution;
-  const int kFrameHeight = frame_buffer_config.vertical_resolution;
-
-  FillRectangle(*pixel_writer,
-                {0, 0},
-                {kFrameWidth, kFrameHeight - 50},
-                kDesktopBGColor);
-  FillRectangle(*pixel_writer,
-                {0, kFrameHeight - 50},
-                {kFrameWidth, 50},
-                {1, 8, 17});
-  FillRectangle(*pixel_writer,
-                {0, kFrameHeight - 50},
-                {kFrameWidth / 5, 50},
-                {80, 80, 80});
-  DrawRectangle(*pixel_writer,
-                {10, kFrameHeight - 40},
-                {30, 30},
-                {160, 160, 160});
+  DrawDesktop(*pixel_writer);
 
   console = new(console_buf) Console{
-    *pixel_writer, kDesktopFGColor, kDesktopBGColor
+    kDesktopFGColor, kDesktopBGColor
   };
+  console->SetWriter(pixel_writer);
   printk("Welcome to MikanOS!\n");
   SetLogLevel(kWarn);
 
@@ -184,9 +166,11 @@ extern "C" void KernelMainNewStack(
   memory_manager->SetMemoryRange(FrameID{1}, FrameID{available_end / kBytesPerFrame});
   // #@@range_end(mark_allocated)
 
-  mouse_cursor = new(mouse_cursor_buf) MouseCursor{
-    pixel_writer, kDesktopBGColor, {300, 200}
-  };
+  if (auto err = InitializeHeap(*memory_manager)) {
+    Log(kError, "failedto allocate pages: %s at %s:%d\n",
+      err.Name(), err.File(), err.Line());
+    exit(1);
+  }
 
   std::array<Message, 32> main_queue_data;
   ArrayQueue<Message> main_queue{main_queue_data};
@@ -266,6 +250,28 @@ extern "C" void KernelMainNewStack(
       }
     }
   }
+
+  const int kFrameWidth = frame_buffer_config.horizontal_resolution;
+  const int kFrameHeight = frame_buffer_config.vertical_resolution;
+
+  auto bgwindow = std::make_shared<Window>(kFrameWidth, kFrameHeight);
+  auto bgwriter = bgwindow->Writer();
+
+  DrawDesktop(*bgwriter);
+  console->SetWriter(bgwriter);
+
+  auto mouse_window = std::make_shared<Window>(kMouseCursorWidth, kMouseCursorHeight);
+  DrawMouseCursor(mouse_window->Writer(), {0, 0});
+
+  layer_manager = new LayerManager;
+  layer_manager->SetWriter(pixel_writer);
+
+  auto bglayer_id = layer_manager->NewLayer().SetWindow(bgwindow).Move({0, 0}).ID();
+  mouse_layer_id = layer_manager->NewLayer().SetWindow(mouse_window).Move({200, 200}).ID();
+
+  layer_manager->UpDown(bglayer_id, 0);
+  layer_manager->UpDown(mouse_layer_id, 1);
+  layer_manager->Draw();
 
   while (true) {
     __asm__("cli");
