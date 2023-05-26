bbf5a598f7c4d539559c411a61b13cfe46af9ba0
takumi <takumidiary.0927@gmail.com>
2023-05-26 23:20:05 +0900

    timer

--- kernel/.main.d
+++ kernel/.main.d
@@ -52,4 +52,5 @@ main.o: main.cpp \
   paging.hpp memory_manager.hpp window.hpp layer.hpp \
   /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/map \
   /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/__tree \
-  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/__node_handle
+  /home/takumi/osbook/devenv/x86_64-elf/include/c++/v1/__node_handle \
+  timer.hpp
